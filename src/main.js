207bce98031e896daf7ceedd60c502338ae26a35
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-02-29 12:42:33 +0200

    Functioning gallery

--- src/main.js
+++ src/main.js
@@ -1,76 +1,7 @@
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-// Описаний у документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// const images = [
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-//     description: 'Container Haulage Freight',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-//     description: 'Aerial Beach View',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-//     description: 'Flower Blooms',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-//     description: 'Alpine Mountains',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-//     description: 'Mountain Lake Sailing',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-//     description: 'Alpine Spring Meadows',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-//     description: 'Nature Landscape',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-//     description: 'Lighthouse Coast Sea',
-//   },
-// ];
 const galleryList = document.querySelector('ul.gallery');
 let query = '';
 const inputQuery = document.getElementById('search-input');
@@ -82,7 +13,7 @@ const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', () => {
   if (query) {
     fetchImages()
-      .then(images => renderImages(images))
+      .then(data => renderImages(data))
       .catch(error => {
         console.log(error);
         iziToast.error({
@@ -93,8 +24,6 @@ searchButton.addEventListener('click', () => {
       });
   }
 });
-
-// const userList = document.querySelector('.user-list');
 
 function fetchImages() {
   return fetch(
@@ -107,29 +36,38 @@ function fetchImages() {
   });
 }
 
-function renderImages(images) {
-  console.log(images);
+function renderImages(data) {
+  galleryList.innerHTML = '';
+  console.log(data);
+  const images = data.hits;
+  if (images.length == 0) {
+    iziToast.error({
+      title: 'Error',
+      message: `Error: No such pictures!`,
+      position: 'topRight',
+    });
+  }
   const galleryMarkup = images
     .map(image => {
-      `<li class="gallery-item">
-  <a class="gallery-link" href="${image.imageURL}">
-    <img
-      class="gallery-image"
-      width="1280"
-      height="100%"
-      src="${image.webformatURL}"
-      data-source="${image.imageURL}"
-      alt="${image.tags}"
-    />
-  </a>
-  </li>`;
+      return `<li class="gallery-item">
+        <a class="gallery-link" href="${image.largeImageURL}">
+          <img
+            class="gallery-image"
+            width="1280"
+            height="100%"
+            src="${image.webformatURL}"
+            data-source="${image.largeImageURL}"
+            alt="${image.tags}"
+          />
+        </a>
+      </li>`;
     })
     .join('');
   galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+  // Initialize SimpleLightbox
+  const galleryCfg = {
+    captionsData: 'alt',
+  };
+  let lightbox = new SimpleLightbox('.gallery a', galleryCfg);
+  lightbox.on('show.simplelightbox', function () {});
 }
-
-const galleryCfg = {
-  captionsData: 'alt',
-};
-let gallery = new SimpleLightbox('.gallery a', galleryCfg);
-gallery.on('show.simplelightbox', function () {});
