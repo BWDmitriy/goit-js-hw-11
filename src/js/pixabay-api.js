88ba13c04b7b2dc2d455b889b753e9830106a4ea
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-03-01 18:42:22 +0200

    fixes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,9 @@
-import {
-  galleryList
-} from "../main";
-import {
-  query
-} from "../main";
-export function fetchImages() {
-  // galleryList.innerHTML = `<p class="loading-msg">Loading images, please wait...</p>`;
+import { galleryList } from '../main';
+import { query } from '../main';
+// Покращення №4: Додано параметр query до функції fetchImages
+export function fetchImages(query) {
+  // Додано логіку для обробки пробілів у пошуковому запиті
+  query = encodeURIComponent(query);
   galleryList.innerHTML = `<div class="loader"></div>`;
   return fetch(
     `https://pixabay.com/api/?key=42609290-856768105ab9e79485c69bf61&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
